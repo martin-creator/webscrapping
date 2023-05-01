7f293c9d005afff897fc3e15bc9c30d2db71740c
martin-creator <martinlubowa@outlook.com>
2023-05-01 11:56:57 +0300

    Stored scrapped data and extracted relevent information

--- simple-scrapper/scrapper.rb
+++ simple-scrapper/scrapper.rb
@@ -4,8 +4,20 @@ require 'nokogiri'
 def scrapping
     html = URI.open('https://github.com/search?q=Ruby+Web+Scraping') # Open the url
     doc = Nokogiri::HTML(html) # Parse the html
+    items_array = [] # Create an empty array to store the items
 
-    puts doc
+    doc.search('.repo-list li').each do |element|
+        name = element.css('a.v-align-middle').text
+        description = element.css('p.mb-1').text.strip
+
+        items_array << [name, description]
+    end
+
+    items_array.each_with_index do |item, index|
+        puts "-----------------------------------"
+        puts "Item #{index + 1} - #{item[0]}"
+        puts "Description: #{item[1]}"
+    end
 
 end
 
