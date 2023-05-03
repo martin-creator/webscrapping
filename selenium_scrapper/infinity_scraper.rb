92b475c63487b99b76c4f26a222f2f133b2ad8e7
martin-creator <martinlubowa@outlook.com>
2023-05-03 21:21:48 +0300

    Successfully implemented infinity using selenium

--- selenium_scrapper/infinity_scraper.rb
+++ selenium_scrapper/infinity_scraper.rb
@@ -10,13 +10,30 @@ driver.get "https://www.rottentomatoes.com/browse/movies_in_theaters/"
 
 movies_name_selector = "div.discovery-tiles div[slot=caption] span.p--small"
 
+loop do
+    # Get the movies number
+    movies_count = driver.find_elements(:css, movies_name_selector).size
+    
+     begin
+        puts "> #{movies_count} movies found so far"
+        button = driver.find_element(:css, "div.discovery_actions button")
+        button.click
+        puts "---Load more button clicked"
+        sleep(30)
+     rescue Exception => e
+        puts "---No more movies to load"
+        break
+     end
+   
+end
+
 count = 0
 driver.find_elements(:css, movies_name_selector ).each do |e|
     name = e.text
 
     puts '---------------------------------------------------'
     puts "#{count += 1} - #{name}"
-
+    
     # "" vs '' - "" allows for string interpolation while '' does not
 end
 
