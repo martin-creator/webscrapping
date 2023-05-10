9c6e1a0368a3ca0b741276651c520aab5142b17e
martin-creator <martinlubowa@outlook.com>
2023-05-10 08:48:28 +0300

    Implemented search api

--- sinatra_api/api.rb
+++ sinatra_api/api.rb
@@ -27,3 +27,32 @@ get '/repos' do
     JSON.pretty_generate(items_array)
 end
 
+get '/search/:query' do
+    url = "https://github.com/search?q=#{params[:query]}"
+    items_array = []
+
+    for page in 1..3
+        html = URI.open("#{url}&p=#{page}")
+        doc = Nokogiri::HTML(html)
+
+        doc.search('.repo-list li').each do |element|
+            name = element.css('a.v-align-middle').text
+            description = element.css('p.mb-1').text.strip
+    
+            repo = {
+                name: name,
+                description: description
+            }
+    
+            items_array << repo
+        end
+
+        # sleep (3)
+
+
+    end
+
+    JSON.pretty_generate(items_array)
+end
+
+
